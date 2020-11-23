5f40d002a7bc1a0f0f7dda8cb348a248edb6355f
willpolitano <willianpolitano@gmail.com>
2020-11-23 17:38:24 -0300

    classes condicionais

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="cliente">
+    <div :class="{'cliente': !isPremium, 'cliente-premium': isPremium}">
         <h1>NOME - {{ cliente.nome }}</h1>
         <hr>
         <p>EMAIL - {{ cliente.email }}</p>
@@ -11,7 +11,8 @@
 export default {
     data() {
         return {
-        }
+            isPremium: false
+        }   
     },
     props : {
         cliente : Object,
@@ -21,8 +22,13 @@ export default {
 </script>
 
 <style scoped>
-    #cliente {
+    .cliente {
         background-color:rgb(82, 49, 114);
         color:rgb(255, 255, 255);
+    }
+
+    .cliente-premium {
+        background-color:rgb(0, 0, 0);
+        color:rgb(219, 216, 9);
     }
 </style>
