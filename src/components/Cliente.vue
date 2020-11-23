af05ac801aa856218b5f24eee1a7f18b9e4c687c
willpolitano <willianpolitano@gmail.com>
2020-11-23 14:45:15 -0300

    meu primeiro componente

---
+++ src/components/Cliente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="cliente">
+        <h2>Cliente aqui !!!!</h2>
+        <h3>Descrição Cliente</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #cliente {
+        background-color:rgb(82, 49, 114);
+        color:rgb(255, 255, 255);
+    }
+</style>
