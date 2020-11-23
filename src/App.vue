4bbe709cd1a1e2fcaabe4d7559dd7b38eda08284
willpolitano <willianpolitano@gmail.com>
2020-11-23 15:23:26 -0300

    data binding

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,15 @@
   <div id="app">
     <h1>Meu primeiro componente</h1>
     <cliente/>
-    <produto/>
   </div>
 </template>
 
 <script>
 import Cliente from './components/Cliente'
-import Produto from './components/Produto'
 export default {
   name: 'App',
   components: {
-    Cliente,
-    Produto
+    Cliente
   }
 }
 </script>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,13 +1,29 @@
 <template>
     <div id="cliente">
         <h2>Cliente aqui !!!!</h2>
-        <h3>Descrição Cliente</h3>
+        <hr>
+        <!-- one way data binding : leitura a variavel -->
+        <input type="text" :value="nome">
+
+        <!-- two way data binding : atualiza a variavel -->
+        <input type="text" v-model="descricao"> 
+        <hr>
+        <h1>NOME - {{ nome }}</h1>
+        <h1>DESCRICAO - {{ descricao }}</h1>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            nome : 'Willian Politano',
+            numero : '',
+            email : '',
+            idade : 0,
+            descricao : 'LOREM IPSUM'
+        }
+    }
 }
 </script>
 
